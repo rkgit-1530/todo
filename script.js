6806e2116fd8e5dde6d76b3c847b1b805ccdb842
rajkaran <160375104+rkgit-1530@users.noreply.github.com>
2024-08-01 09:55:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 // selecting popupbox,overlay,addbutton
-var popupbox = document.querySelector(".popupbox")
-var overlay = document.querySelector(".overlay")
-var add = document.getElementById("add")
-var cancel = document.getElementById("Cancel")
-var container = document.querySelector("#cardBox")
-var addtodo = document.getElementById("Addtodo")
+var popupbox = document.querySelector(".popupbox");
+var overlay = document.querySelector(".overlay");
+var add = document.getElementById("add");
+var cancel = document.getElementById("Cancel");
+var container = document.querySelector("#cardBox");
+var addtodo = document.getElementById("Addtodo");
 
 const detailsForm = document.querySelector('#detailForm');
 const cards = document.querySelectorAll('#card');
-const deleteButtons = document.querySelectorAll('#deleteCard')
+const deleteButtons = document.querySelectorAll('#deleteCard');
 
 
 add.addEventListener("click", function () {
-    popupbox.style.display = "block"
-    overlay.style.display = "block"
+    popupbox.style.display = "block";
+    overlay.style.display = "block";
 })
 
 cancel.addEventListener("click", function () {
-    popupbox.style.display = "none"
-    overlay.style.display = "none"
+    popupbox.style.display = "none";
+    overlay.style.display = "none";
 })
 
 
 
 detailsForm.addEventListener("submit", function (event) {
     event.preventDefault()
-    var title = document.getElementById("Title")
+    var title = document.getElementById("Title");
     var details = document.getElementById("Todo");
 
     const div = document.createElement("div");
@@ -35,12 +35,12 @@ detailsForm.addEventListener("submit", function (event) {
     const button = document.createElement('button');
 
     div.setAttribute('class', 'card text-bg-success mb-3');
-    div.setAttribute('id', 'card')
-    header.setAttribute('class', 'card-header h2 text-center')
-    footer.setAttribute('class', 'card-body')
-    p.setAttribute('class', 'card-text')
+    div.setAttribute('id', 'card');
+    header.setAttribute('class', 'card-header h2 text-center');
+    footer.setAttribute('class', 'card-body');
+    p.setAttribute('class', 'card-text');
     button.setAttribute('class', 'btn btn-dark');
-    button.setAttribute('id', 'deleteCard')
+    button.setAttribute('id', 'deleteCard');
 
     button.innerText = <i class="bi bi-trash"></i>'delete';
     header.innerHTML = title.value;
@@ -59,9 +59,9 @@ detailsForm.addEventListener("submit", function (event) {
     div.append(header);
     div.append(footer);
 
-    container.append(div)
-    popupbox.style.display = "none"
-    overlay.style.display = "none"
+    container.append(div);
+    popupbox.style.display = "none";
+    overlay.style.display = "none";
 })
 
 
